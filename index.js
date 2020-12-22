56dab35bedb0c8e25d5115b03aa0881888beeb7d
khiemkino <khiemkino@gmail.com>
2020-12-23 06:31:03 +0700

    newUpdate

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const RNI18n = require('react-native').NativeModules.RNI18n;
 const I18nJs = require('i18n-js');
 
 if (typeof RNI18n !== 'undefined') {
-  I18nJs.locale = RNI18n.languages[0];
 } else if (__DEV__) {
   console.warn('react-native-i18n module is not correctly linked');
 }
